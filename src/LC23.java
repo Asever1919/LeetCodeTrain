40e4a5baa1e4c251128bdadb8ae746fd81d13e74
zhangshuai17 <zhangshuai17@kingsoft.com>
2023-07-16 12:18:48 +0800

    LC23题提交，多个升序链表排序，困难题。

---
+++ src/LC23.java
@@ -0,0 +1,26 @@
+import com.sun.org.apache.bcel.internal.generic.LNEG;
+
+public class LC23 {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) return null;
+        ListNode res = lists[0];
+        for (int i = 1; i < lists.length; i++) {
+            res = mergeTwoLists(res, lists[i]);
+        }
+        return res;
+    }
+
+    private ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) {
+            return list2;
+        } else if(list2 == null) {
+            return list1;
+        } else if (list1.val <= list2.val){
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
+        }
+    }
+}
