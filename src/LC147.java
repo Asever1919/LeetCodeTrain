5f23d801e2651eb535342696c2e4096b0aed0252
zhangshuai17 <zhangshuai17@kingsoft.com>
2023-07-16 13:35:43 +0800

    LC147:提交，之前的是有问题的

--- src/LC147.java
+++ src/LC147.java
@@ -1,20 +1,54 @@
+import com.sun.org.apache.bcel.internal.generic.LNEG;
+
 public class LC147 {
-    public ListNode detectCycle(ListNode head) {
-        ListNode fast = head;
-        ListNode slow = head;
-        while (fast != null && fast.next != null) {
-            fast = fast.next.next;
-            slow = slow.next;
-            if (fast == slow) {
-                ListNode index1 = head;
-                ListNode index2 = slow;
-                while (index1 != index2) {
-                    index1 = index1.next;
-                    index2 = index2.next;
-                }
-                return index1;
+//    public static void main(String[] args) {
+//        LC147 lc147 = new LC147();
+//        int[] nums = {4, 2, 1, 3};
+//        ListNode head = lc147.buildListNode(nums);
+//        head = lc147.insertionSortList(head);
+//        System.out.println("===");
+//    }
+
+//    private ListNode buildListNode(int[] nums) {
+//        ListNode head = new ListNode(nums[0]);
+//        ListNode cur = head;
+//        for (int i = 1; i < nums.length; i++) {
+//            cur.next = new ListNode(nums[i]);
+//            cur = cur.next;
+//        }
+//        return head;
+//    }
+    public ListNode insertionSortList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode dummy = new ListNode(-5001);
+        dummy.next = head;
+        ListNode pre = dummy;
+        ListNode cur = head;
+        while (cur != null) {
+            if (cur.val >= pre.val) {
+                pre = cur;
+                cur = cur.next;
+            } else {
+                ListNode temp = cur;
+                cur = cur.next;
+                pre.next = cur;
+                temp.next = null;
+                insertNode(dummy, temp);
             }
         }
-        return null;
+        return dummy.next;
     }
+
+    private void insertNode(ListNode dummy, ListNode temp) {
+        if (dummy.next == null || temp == null) return;
+        ListNode nodeH = dummy.next;
+        ListNode preH = dummy;
+        while (nodeH.val < temp.val && nodeH != null) {
+            preH = nodeH;
+            nodeH = nodeH.next;
+        }
+        temp.next = nodeH;
+        preH.next = temp;
+    }
+
 }
